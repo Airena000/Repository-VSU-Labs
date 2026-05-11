fa1d430c97af9a16d5cc8bf73fbe55378395e825
Airena000 <lizahotejkina@gmai.com>
2026-05-11 10:45:56 +0300

    Добавлена начальная информация

--- Lab_8/main.cpp
+++ Lab_8/main.cpp
@@ -1,4 +1,4 @@
-// РџСЂРё РІС‹РїРѕР»РЅРµРЅРёРё Р·Р°РґР°РЅРёСЏ СЃРІРѕРµРіРѕ РІР°СЂРёР°РЅС‚Р° РЅСѓР¶РЅРѕ РІРїРёСЃР°С‚СЊ РЅРѕРјРµСЂ Рё РЅР°Р·РІР°РЅРёРµ РІР°СЂРёР°РЅС‚Р°, Р° С‚Р°РєР¶Рµ СЃРІРѕРё РёРјСЏ Рё С„Р°РјРёР»РёСЋ
+// При выполнении задания своего варианта нужно вписать номер и название варианта, а также свои имя и фамилию
 
 #include <iostream>
 #include <fstream>
@@ -9,20 +9,20 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	ifstream file("library.txt");
 	if (!file.is_open()) {
-		cout << "РћС€РёР±РєР° РѕС‚РєСЂС‹С‚РёСЏ С„Р°Р№Р»Р°!" << endl;
+		cout << "Ошибка открытия файла!" << endl;
 		return 1;
 	}
 	string familiya, name, otchestvo, start_date, end_date, author_fam, author_name, author_otch, book_name;
-	cout << "РђР±РѕРЅРµРјРµРЅС‚С‹ СЃ РєРЅРёРіР°РјРё РџСѓС€РєРёРЅР°: " << endl;
+	cout << "Абонементы с книгами Пушкина: " << endl;
 	while (file >> familiya >> name >> otchestvo >> start_date >> end_date >> author_fam >> author_name >> author_otch>>book_name) {
-		if (author_fam == "РџСѓС€РєРёРЅ" && author_name == "РђР»РµРєСЃР°РЅРґСЂ" && author_otch == "РЎРµСЂРіРµРµРІРёС‡") {
+		if (author_fam == "Пушкин" && author_name == "Александр" && author_otch == "Сергеевич") {
 			cout << familiya << " " << name << " " << otchestvo << ", " << start_date << ", " << end_date << ", " <<
 				author_fam << " " << author_name << " " << author_otch << ", " << book_name << endl;
 		}
 	}
 	file.close();
 	file.open("library.txt");
-	cout << "РђР±РѕРЅРµРјРµРЅС‚С‹ СЃ РєРЅРёРіР°РјРё РІР·СЏС‚С‹РјРё РІ РјР°СЂС‚Рµ 2015:" << endl;
+	cout << "Абонементы с книгами взятыми в марте 2015:" << endl;
 	while (file >> familiya >> name >> otchestvo >> start_date >> end_date >> author_fam >> author_name >> author_otch >> book_name) {
 		if (start_date[3] == '0' && start_date[4] == '3' && start_date[6] == '1' && start_date[7] == '5') {
 			cout << familiya << " " << name << " " << otchestvo << ", " << start_date << ", " << end_date << ", " <<
