f3807bdffa2aad27087f0d6a36f8287c7281dd44
Airena000 <lizahotejkina@gmai.com>
2026-05-11 10:52:53 +0300

    Добавлены заголовочные файлы

---
+++ Lab_8/book.h
@@ -0,0 +1,9 @@
+#ifndef BOOK_H
+#define BOOK_H
+#include <string>
+using namespace std;
+struct BOOK {
+	string familiya, name, otchestvo, start_date, end_date, author_fam, author_name, author_otch, book_name;
+};
+
+#endif
