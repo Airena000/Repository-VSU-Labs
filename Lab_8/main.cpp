f5d8f40609be5b8a339f33297c73672aa5c3b971
Airena000 <lizahotejkina@gmai.com>
2026-05-11 10:34:57 +0300

    Код готов

--- Lab_8/main.cpp
+++ Lab_8/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-
+	setlocale(LC_ALL, "Russian");
 	ifstream file("library.txt");
 	if (!file.is_open()) {
 		cout << "Ошибка открытия файла!" << endl;
@@ -14,11 +14,21 @@ int main() {
 	cout << "Абонементы с книгами Пушкина: " << endl;
 	while (file >> familiya >> name >> otchestvo >> start_date >> end_date >> author_fam >> author_name >> author_otch>>book_name) {
 		if (author_fam == "Пушкин" && author_name == "Александр" && author_otch == "Сергеевич") {
-			cout << familiya << " ," << name << " ," << otchestvo << " ," << start_date << " ," << end_date << " ," <<
-				author_fam << " ," << author_name << " ," << author_otch << " ," << book_name << endl;
+			cout << familiya << " " << name << " " << otchestvo << ", " << start_date << ", " << end_date << ", " <<
+				author_fam << " " << author_name << " " << author_otch << ", " << book_name << endl;
 		}
 	}
 	file.close();
+	file.open("library.txt");
+	cout << "Абонементы с книгами взятыми в марте 2015:" << endl;
+	while (file >> familiya >> name >> otchestvo >> start_date >> end_date >> author_fam >> author_name >> author_otch >> book_name) {
+		if (start_date[3] == '0' && start_date[4] == '3' && start_date[6] == '1' && start_date[7] == '5') {
+			cout << familiya << " " << name << " " << otchestvo << ", " << start_date << ", " << end_date << ", " <<
+				author_fam << " " << author_name << " " << author_otch << ", " << book_name << endl;
+		}
+	}
 
+	file.close();
+	
 	return 0;
 }
