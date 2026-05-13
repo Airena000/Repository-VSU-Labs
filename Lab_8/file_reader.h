1622be69ca1df99b496530dd013e5d106afec1c5
Airena000 <lizahotejkina@gmai.com>
2026-05-13 11:11:48 +0300

    Добавлены сортировки

--- Lab_8/file_reader.h
+++ Lab_8/file_reader.h
@@ -8,4 +8,12 @@ using namespace std;
 
 vector<CONFERENCE> readFile(const string& filename);
 void filter(const vector<CONFERENCE>& reports, int choice);
+typedef int (*CompareFunc)(const CONFERENCE*, const CONFERENCE*);
+
+void heapSort(CONFERENCE* arr[], int size, CompareFunc cmp);
+void mergeSort(CONFERENCE* arr[], int left, int right, CompareFunc cmp);
+int compareByDuration(const CONFERENCE* a, const CONFERENCE* b);
+int compareByAuthor(const CONFERENCE* a, const CONFERENCE* b);
+int getDuration(const CONFERENCE& r);
+
 #endif
