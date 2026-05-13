b06dca3b0c6ae71d8829b7fc4cc8ccd180e8a730
Airena000 <lizahotejkina@gmai.com>
2026-05-13 09:38:09 +0300

    Добавлена фильтрация

--- Lab_8/file_reader.cpp
+++ Lab_8/file_reader.cpp
@@ -25,3 +25,44 @@ vector<BOOK> readFile(const string& filename) {
     }
     return records;
 }
+
+void filter(const vector<BOOK>& records,
+    int choice,
+    const string& author_fam,
+const string& author_name,
+const string& author_otch,
+int targetMonth,
+int targetYear) {
+
+    switch (choice) {
+    case 1: {
+        // Фильтрация по автору (Пушкин)
+        cout << "=== Книги Пушкина А.С. ===" << endl;
+        for (const auto& rec : records) {
+            if (rec.author_fam == author_fam&&
+                rec.author_name == author_name &&
+                rec.author_otch == author_otch) {
+                cout << rec.familiya << " " << rec.name << " "
+                    << rec.otchestvo << " - " << rec.book_name << endl;
+            }
+        }
+        break;
+    }
+    case 2: {
+        // Фильтрация по дате (март 2015)
+        cout << "=== Записи за март 2015 года ===" << endl;
+        for (const auto& rec : records) {
+            int month = (rec.start_date[3] - '0') * 10 + (rec.start_date[4] - '0');
+            int year = (rec.start_date[6] - '0') * 10 + (rec.start_date[7] - '0');
+            if (month == targetMonth && year == targetYear) {
+                cout << rec.familiya << " " << rec.name << " "
+                    << rec.otchestvo << " - "<< rec.book_name  << endl;
+            }
+        }
+        break;
+    }
+    default:
+        cout << "Неверный выбор!" << endl;
+        break;
+    }
+}
