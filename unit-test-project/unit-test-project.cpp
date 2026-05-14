42cfdd8d18a0bbd850f51705b3dcc6b237366ee2
Airena000 <lizahotejkina@gmai.com>
2026-05-14 12:28:24 +0300

    Все тесты пройдены

--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -10,41 +10,41 @@ namespace unittestproject
 	{
 	public:
 		
-		TEST_METHOD(Test_Data) // РѕР±С‹С‡РЅС‹Р№
+		TEST_METHOD(Test_Data) // обычный
 		{
 			vector <CONFERENCE> reports;
 			CONFERENCE r1;
 			r1.start = "09:00";
 			r1.end = "09:20";
-			r1.surname = "РРІР°РЅРѕРІ";
+			r1.surname = "Иванов";
 			reports.push_back(r1);
 
 			CONFERENCE r2;
-			r1.start = "10:00";
-			r1.end = "10:40";
-			r1.surname = "РџРµС‚СЂРѕРІ";
+			r2.start = "10:00";
+			r2.end = "10:40";
+			r2.surname = "Петров";
 			reports.push_back(r2);
 			
 			int result = process(reports);
 			Assert::AreEqual(40, result);
 		}
-		TEST_METHOD(Test_Empty) { // РїСѓСЃС‚РѕР№ 
+		TEST_METHOD(Test_Empty) { // пустой 
 			vector <CONFERENCE> reports;
 			int result = process(reports);
 			Assert::AreEqual(0, result);
 		}
-		TEST_METHOD(Test_Equal) { // РґР»СЏ РѕРґРёРЅР°РєРѕРІС‹С… Р·РЅР°С‡РµРЅРёР№
+		TEST_METHOD(Test_Equal) { // для одинаковых значений
 			vector <CONFERENCE> reports;
 			CONFERENCE r1;
 			r1.start = "09:00";
 			r1.end = "09:20";
-			r1.surname = "РРІР°РЅРѕРІ";
+			r1.surname = "Иванов";
 			reports.push_back(r1);
 
 			CONFERENCE r2;
-			r1.start = "10:00";
-			r1.end = "10:20";
-			r1.surname = "РџРµС‚СЂРѕРІ";
+			r2.start = "10:00";
+			r2.end = "10:20";
+			r2.surname = "Петров";
 			reports.push_back(r2);
 
 			int result = process(reports);
