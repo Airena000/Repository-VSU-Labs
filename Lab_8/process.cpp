a2114a516a0cbf9f7b1a4cfe460ff83fbf789470
Airena000 <lizahotejkina@gmai.com>
2026-05-14 11:55:02 +0300

    Добавлены тесты

--- Lab_8/process.cpp
+++ Lab_8/process.cpp
@@ -1,6 +1,5 @@
 #include "processing.h"
 
 int process(const vector <CONFERENCE>& reports) { // заглушка
-
 	return 0;
 }
--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -1,17 +1,54 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-
+#include "../Lab_8/processing.h"
+#include "../Lab_8/conference.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
+using namespace std;
 namespace unittestproject
 {
 	TEST_CLASS(unittestproject)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Test_Data) // обычный
 		{
-			Assert::Fail();
+			vector <CONFERENCE> reports;
+			CONFERENCE r1;
+			r1.start = "09:00";
+			r1.end = "09:20";
+			r1.surname = "Иванов";
+			reports.push_back(r1);
+
+			CONFERENCE r2;
+			r1.start = "10:00";
+			r1.end = "10:40";
+			r1.surname = "Петров";
+			reports.push_back(r2);
+			
+			int result = process(reports);
+			Assert::AreEqual(40, result);
+		}
+		TEST_METHOD(Test_Empty) { // пустой 
+			vector <CONFERENCE> reports;
+			int result = process(reports);
+			Assert::AreEqual(0, result);
+		}
+		TEST_METHOD(Test_Equal) { // для одинаковых значений
+			vector <CONFERENCE> reports;
+			CONFERENCE r1;
+			r1.start = "09:00";
+			r1.end = "09:20";
+			r1.surname = "Иванов";
+			reports.push_back(r1);
+
+			CONFERENCE r2;
+			r1.start = "10:00";
+			r1.end = "10:20";
+			r1.surname = "Петров";
+			reports.push_back(r2);
+
+			int result = process(reports);
+			Assert::AreEqual(20, result);
 		}
 	};
 }
